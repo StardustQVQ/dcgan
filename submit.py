7faab396f4ce19e3723ee72bab6c04146fb930cc
Stardust <34624141+StardustQVQ@users.noreply.github.com>
2021-01-22 00:31:11 +0800

    Delete submit.py

--- submit.py
+++
@@ -1,5 +0,0 @@
-import os
-from flyai.train_helper import submit, upload_data
-
-# upload_data("./data/cats.zip", dir_name="/data", overwrite=True)
-submit(train_name="train_cat_data", code_path=os.curdir, cmd="python train.py")
